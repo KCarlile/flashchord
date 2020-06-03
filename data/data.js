f23eaed96936bdbd6c54714c6208af20ec53e1cf
Kenny Carlile <kennycarlile@gmail.com>
2020-06-02 22:09:07 -0700

    Next chord added, foundations for key/chords in place. Sharps, flats, dim fixed.

--- data/data.js
+++ data/data.js
@@ -1,35 +1,43 @@
-var $chromatic = ["A", "Ab", "A#", "B", "Bb", "B#", "C", "Cb", "C#", "D", "Db", "D#", "E", "Eb", "E#", "F", "Fb", "F#", "G", "Gb", "G#"];
+var $chromatic = ["A", "A♭", "A♯", "B", "B♭", "B♯", "C", "C♭", "C♯", "D", "D♭", "D♯", "E", "E♭", "E♯", "F", "F♭", "F♯", "G", "G♭", "G♯"];
 var $quality_beginner = ["", "m", "m7", "7"];
-var $quality_intermediate = $quality_beginner.concat(["+", "dim", "maj7", "m7b5", "6", "m6", "sus2", "sus4", "9", "m9", "11", "13", "6/9"]);
+var $quality_intermediate = $quality_beginner.concat(["+", "°", "maj7", "m7♭5", "6", "m6", "sus2", "sus4", "9", "m9", "11", "13", "6/9"]);
 var $quality_advanced = $quality_intermediate.concat(["7alt"]);
-var $extension = ["", "(#9)", "(b9)", "(#5)", "(b5)"];
+var $extension = ["", "(♯9)", "(♭9)", "(♯5)", "(♭5)"];
 
 var $keys = {
-  "Gb Major":   ["Gb", "Ab", "Bb", "Cb", "Db", "Eb", "F"],
-  "Db Major":   ["Db", "Eb", "F", "Gb", "Ab", "Bb", "C"],
-  "Ab Major":   ["Ab", "Bb", "C", "Db", "Eb", "F", "G"],
-  "Eb Major":   ["Eb", "F", "G", "Ab", "Bb", "C", "D"],
-  "Bb Major":   ["Bb", "C", "D", "Eb", "F", "G", "A"],
-  "F Major":    ["F", "G", "A", "Bb", "C", "D", "E"],
+  "G♭ Major":   ["G♯", "A♭", "B♭", "C♭", "D♭", "E♭", "F"],
+  "D♭ Major":   ["D♭", "E♭", "F", "G♭", "A♭", "B♭", "C"],
+  "A♭ Major":   ["A♭", "B♭", "C", "D♭", "E♭", "F", "G"],
+  "E♭ Major":   ["E♭", "F", "G", "A♭", "B♭", "C", "D"],
+  "B♭ Major":   ["B♭", "C", "D", "E♭", "F", "G", "A"],
+  "F Major":    ["F", "G", "A", "B♭", "C", "D", "E"],
   "C Major":    ["C", "D", "E", "F", "G", "A", "B"],
-  "G Major":    ["G", "A", "B", "C", "D", "E", "F#"],
-  "D Major":    ["D", "E", "F#", "G", "A", "B", "C#"],
-  "A Major":    ["A", "B", "C#", "D", "E", "F#", "G#"],
-  "E Major":    ["E", "F#", "G#", "A", "B", "C#", "D#"],
-  "B Major":    ["B", "C#", "D#", "E", "F#", "G#", "A#"],
-  "F# Major":   ["F#", "G#", "A#", "B", "C#", "D#", "E#"],
+  "G Major":    ["G", "A", "B", "C", "D", "E", "F♯"],
+  "D Major":    ["D", "E", "F♯", "G", "A", "B", "C♯"],
+  "A Major":    ["A", "B", "C♯", "D", "E", "F♯", "G♯"],
+  "E Major":    ["E", "F♯", "G♯", "A", "B", "C♯", "D♯"],
+  "B Major":    ["B", "C♯", "D♯", "E", "F♯", "G♯", "A♯"],
+  "F♯ Major":   ["F♯", "G♯", "A♯", "B", "C♯", "D♯", "E♯"],
 
-  "Eb Minor":   ["Eb", "F", "Gb", "Ab", "Bb", "Cb", "Db"],
-  "Bb Minor":   ["Bb", "C", "Db", "Eb", "F", "Gb", "Ab"],
-  "F Minor":    ["F", "G", "Ab", "Bb", "C", "Db", "Eb"],
-  "C Minor":    ["C", "D", "Eb", "F", "G", "Ab", "Bb"],
-  "G Minor":    ["G", "A", "Bb", "C", "D", "Eb", "F"],
-  "D Minor":    ["D", "E", "F", "G", "A", "Bb", "C"],
+  "E♭ Minor":   ["E♭", "F", "G♭", "A♭", "B♭", "C♭", "D♭"],
+  "B♭ Minor":   ["B♭", "C", "D♭", "E♭", "F", "G♭", "A♭"],
+  "F Minor":    ["F", "G", "A♭", "B♭", "C", "D♭", "E♭"],
+  "C Minor":    ["C", "D", "E♭", "F", "G", "A♭", "B♭"],
+  "G Minor":    ["G", "A", "B♭", "C", "D", "E♭", "F"],
+  "D Minor":    ["D", "E", "F", "G", "A", "B♭", "C"],
   "A Minor":    ["A", "B", "C", "D", "E", "F", "G"],
-  "E Minor":    ["E", "F#", "G", "A", "B", "C", "D"],
-  "B Minor":    ["B", "C#", "D", "E", "F#", "G", "A"],
-  "F# Minor":   ["F#", "G#", "A", "B", "C#", "D", "E"],
-  "C# Minor":   ["C#", "D#", "E", "F#", "G#", "A", "B"],
-  "G# Minor":   ["G#", "A#", "B", "C#", "D#", "E", "F#"],
-  "D# Minor":   ["D#", "E#", "F#", "G#", "A#", "B", "C#"],
+  "E Minor":    ["E", "F♯", "G", "A", "B", "C", "D"],
+  "B Minor":    ["B", "C♯", "D", "E", "F♯", "G", "A"],
+  "F♯ Minor":   ["F♯", "G♯", "A", "B", "C♯", "D", "E"],
+  "C♯ Minor":   ["C♯", "D♯", "E", "F♯", "G♯", "A", "B"],
+  "G♯ Minor":   ["G♯", "A♯", "B", "C♯", "D♯", "E", "F♯"],
+  "D♯ Minor":   ["D♯", "E♯", "F♯", "G♯", "A♯", "B", "C♯"],
 };
+
+// flat: &#9837; ♯
+// sharp: &#9839; ♭
+// natural: &#9838; ♮
+// diminished: &#176; °
+// double sharp: 𝄪
+// double flat: 𝄫
+// major: Δ
--- js/flashchords.js
+++ js/flashchords.js
@@ -1,5 +1,7 @@
 var $intervalId;
 var $DELAY = 1000 * 4; // 1000ms * SECONDS
+var $chord;
+var $next_chord;
 
 $(document).ready(function(){
     // slider setup
@@ -29,23 +31,11 @@ $(document).ready(function(){
 
     // chord change
     var changeChord = function() {
-      var $difficulty = $('input[name="difficulty"]:checked').val();
-      var $notes = getKey();
-      console.log($notes);
-      var $quality;
-      var $ext;
+      $chord = $next_chord ? $next_chord : getChord();
+      $next_chord = getChord();
 
-      if ($difficulty == "beginner") {
-        $quality = getRandom($quality_beginner);
-        $ext = "";
-      } else if ($difficulty == "intermediate") {
-        $quality = getRandom($quality_intermediate);
-        $ext = "";
-      } else if ($difficulty == "advanced") {
-        $quality = getRandom($quality_advanced);
-        $ext = getRandom($extension)
-      }
-      $("#chord_name").text(getRandom($notes) + $quality + $ext);
+      $("#chord_name").text($chord);
+      $("#next_chord_name").text($next_chord);
     }
 
     function chordTimer() {
@@ -55,13 +45,6 @@ $(document).ready(function(){
         }, $DELAY);
     }
 });
-
-/*
-function getDelay() {
-    $DELAY = $("#slider").slider("value") * 1000;
-    return $DELAY;
-}
-*/
 
 function getRandom ($array) {
     return $array[Math.floor(Math.random() * $array.length)];
@@ -82,4 +65,91 @@ function getKey() {
   console.log($key_notes);
 
   return $key_notes;
+}
+
+function getChord() {
+  var $difficulty = $('input[name="difficulty"]:checked').val();
+  var $key = getKey();
+  var $note = getRandom($key);
+
+  console.log($key + " :: " + $note + " (" + $key.indexOf($note) + ")");
+
+  if ($("#keys").val().indexOf("Major") >= 0) {
+    // major
+    $chord_quality = getHarmonicQualityMajor($key.indexOf($note));
+  } else if ($("#keys").val().indexOf("Minor") >= 0) {
+    // major
+    $chord_quality = getHarmonicQualityMinor($key.indexOf($note));
+  }
+  else { $chord_quality = "BLEH!"; }
+
+  console.log("Harmonic Quality: " + $chord_quality);
+
+  var $quality;
+  var $ext;
+
+  if ($difficulty == "beginner") {
+    $quality = getRandom($quality_beginner);
+    $ext = "";
+  } else if ($difficulty == "intermediate") {
+    $quality = getRandom($quality_intermediate);
+    $ext = "";
+  } else if ($difficulty == "advanced") {
+    $quality = getRandom($quality_advanced);
+    $ext = getRandom($extension)
+  }
+
+  return $note + $quality + $ext;
+}
+
+function getHarmonicQualityMajor($scale_tone) {
+  var $quality;
+  switch($scale_tone) {
+    case 0:
+    case 3:
+    case 4:
+      // major
+      $quality = "";
+      break;
+    case 1:
+    case 2:
+    case 5:
+      // minor
+      $quality = "m";
+      break;
+    case 6:
+      // diminished
+      $quality = "dim";
+      break;
+    default:
+      $quality = "";
+  }
+
+  return $quality;
+}
+
+function getHarmonicQualityMinor($scale_tone) {
+  var $quality;
+  switch($scale_tone) {
+    case 2:
+    case 5:
+    case 6:
+      // major
+      $quality = "";
+      break;
+    case 0:
+    case 3:
+    case 4:
+      // minor
+      $quality = "m";
+      break;
+    case 1:
+      // diminished
+      $quality = "dim";
+      break;
+    default:
+      $quality = "";
+  }
+
+  return $quality;
 }
