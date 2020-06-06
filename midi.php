839f3dfc3604b5451e12ffa02f0be048daa032b5
Kenny Carlile <kennycarlile@gmail.com>
2020-06-06 09:19:11 -0700

    Adding page for experimenting with MIDI.

---
+++ midi.php
@@ -0,0 +1,24 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <?php require 'components/head.php'; ?>
+    <title>MIDI Test | Flash Chord</title>
+    <script>
+
+
+    </script>
+  </head>
+  <body>
+    <?php require 'components/header.php'; ?>
+    <div class="container">
+        <p class="lead">MIDI Test</p>
+    </div>
+    <div class="container">
+    </div>
+    <footer class="pt-4 my-md-5 pt-md-5 border-top">
+        <div class="container">
+        </div>
+    </footer>
+    <?php require 'components/footer.php'; ?>
+  </body>
+</html>
