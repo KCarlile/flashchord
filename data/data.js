a79b9e95942081819b52b5f094b326c2c8f2ba20
Don-Stevenson <dsteven1@gmail.com>
2020-07-20 20:20:46 -0400

    update from var to const

--- data/data.js
+++ data/data.js
@@ -1,15 +1,15 @@
-var $chromatic = ["A", "A♭", "A♯", "B", "B♭", "B♯", "C", "C♭", "C♯", "D", "D♭", "D♯", "E", "E♭", "E♯", "F", "F♭", "F♯", "G", "G♭", "G♯"];
+const $chromatic = ["A", "A♭", "A♯", "B", "B♭", "B♯", "C", "C♭", "C♯", "D", "D♭", "D♯", "E", "E♭", "E♯", "F", "F♭", "F♯", "G", "G♭", "G♯"];
 
-var $major_chords = ["", "maj7", "6", "sus2", "sus4", "6/9"];
-var $minor_chords = ["m", "m7", "m9", "m11"];
-var $diminished_chords = ["°", "°7", "m7♭5"];
-var $augmented_chords = ["+", "7alt"];
-var $dominant_chords = ["7", "9", "11", "13"];
-var $all_chords = [].concat($major_chords, $minor_chords, $diminished_chords, $augmented_chords, $dominant_chords);
+const $major_chords = ["", "maj7", "6", "sus2", "sus4", "6/9"];
+const $minor_chords = ["m", "m7", "m9", "m11"];
+const $diminished_chords = ["°", "°7", "m7♭5"];
+const $augmented_chords = ["+", "7alt"];
+const $dominant_chords = ["7", "9", "11", "13"];
+const $all_chords = [].concat($major_chords, $minor_chords, $diminished_chords, $augmented_chords, $dominant_chords);
 
-var $extension = ["", "(♯5)", "(♭5)", "(♯9)", "(♭9)", "(♯11)", "(♯13)", "(♭13)"];
+const $extension = ["", "(♯5)", "(♭5)", "(♯9)", "(♭9)", "(♯11)", "(♯13)", "(♭13)"];
 
-var $keys = {
+const $keys = {
     "G♭ Major":   ["G♯", "A♭", "B♭", "C♭", "D♭", "E♭", "F"],
     "D♭ Major":   ["D♭", "E♭", "F", "G♭", "A♭", "B♭", "C"],
     "A♭ Major":   ["A♭", "B♭", "C", "D♭", "E♭", "F", "G"],
