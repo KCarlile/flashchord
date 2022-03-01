a7d0dbe2f0fb5f1bf0c50cc9bb179e17bc491b74
Kenny Carlile <kennycarlile@gmail.com>
2022-02-28 23:30:11 -0800

    Adding theoretical keys and slash degrees.

--- data/data.js
+++ data/data.js
@@ -1,4 +1,5 @@
-var $chromatic = ["A", "A♭", "A♯", "B", "B♭", "B♯", "C", "C♭", "C♯", "D", "D♭", "D♯", "E", "E♭", "E♯", "F", "F♭", "F♯", "G", "G♭", "G♯"];
+/*var $chromatic = ["A", "A♭", "A♯", "B", "B♭", "B♯", "C", "C♭", "C♯", "D", "D♭", "D♯", "E", "E♭", "E♯", "F", "F♭", "F♯", "G", "G♭", "G♯"];*/
+var $chromatic = ["A♭", "A", "A♯", "B♭", "B", "B♯", "C♭", "C", "C♯", "D♭", "D", "D♯", "E♭", "E", "E♯", "F♭", "F", "F♯", "G♭", "G", "G♯"];
 
 var $major_chords = ["", "maj7", "6", "sus2", "sus4", "6/9"];
 var $minor_chords = ["m", "m7", "m9", "m11"];
@@ -39,8 +40,30 @@ var $keys = {
     "D♯ Minor":   ["D♯", "E♯", "F♯", "G♯", "A♯", "B", "C♯"],
 };
 
-// flat: &#9837; ♯
-// sharp: &#9839; ♭
+var $theoretical_keys = {
+    "C♯ Major":   ["C♯", "D♯", "E♯", "F♯", "G♯", "A♯", "B♯"],
+    "G♯ Major":   ["G♯", "A♯", "B♯", "C♯", "D♯", "E♯", "F𝄪"],
+    "D♯ Major":   ["D♯", "E♯", "F𝄪", "G♯", "A♯", "B♯", "C𝄪"],
+    "A♯ Major":   ["A♯", "B♯", "C𝄪", "D♯", "E♯", "F𝄪", "G𝄪"],
+    "E♯ Major":   ["E♯", "F𝄪", "G𝄪", "A♯", "B♯", "C𝄪", "D𝄪"],
+    "B♯ Major":   ["B♯", "C𝄪", "D𝄪", "E♯", "F𝄪", "G𝄪", "A𝄪"],
+    "C♭ Major":   ["C♭", "D♭", "E♭", "F♭", "G♭", "A♭", "B♭"],
+    "F♭ Major":   ["F♭", "G♭", "A♭", "B𝄫", "C♭", "D♭", "E♭"],
+
+    "A♯ Minor":    ["A♯", "B♯", "C♯", "D♯", "E♯", "F♯", "G♯"],
+    "E♯ Minor":    ["E♯", "F𝄪", "G♯", "A♯", "B♯", "C♯", "D♯"],
+    "B♯ Minor":    ["B♯", "C𝄪", "D♯", "E♯", "F𝄪", "G♯", "A♯"],
+    "A♭ Minor":    ["A♭", "B♭", "C♭", "D♭", "E♭", "F♭", "G♭"],
+    "C♭ Minor":    ["C♭", "D♭", "E𝄫", "F♭", "G♭", "A𝄫", "B𝄫"],
+    "D♭ Minor":    ["D♭", "E♭", "F♭", "G♭", "A♭", "B𝄫", "C♭"],
+    "F♭ Minor":    ["F♭", "G♭", "A𝄫", "B𝄫", "C♭", "D𝄫", "E𝄫"],
+    "G♭ Minor":    ["G♭", "A♭", "B𝄫", "C♭", "D♭", "E𝄫", "F♭"],
+}
+
+var $slash_degrees = [0, 2, 4];
+
+// flat: &#9837; ♭
+// sharp: &#9839; ♯
 // natural: &#9838; ♮
 // diminished: &#176; °
 // double sharp: 𝄪
