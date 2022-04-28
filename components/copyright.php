4d75e2ca1ef685facab4b5435fe159dc9e2b8f83
Kenny Carlile <kennycarlile@gmail.com>
2022-04-28 16:16:58 -0700

    Changing copyright link from kennycarlile.com to kcarlile.com.

--- components/copyright.php
+++ components/copyright.php
@@ -1,1 +1,1 @@
-<small class="d-block mb-3 text-muted">©<?php echo date("Y"); ?> FlashChord.com | <a href="http://www.kennycarlile.com/">Kenny Carlile</a> | <a href="/donate">Support Flash Chord</a>
+<small class="d-block mb-3 text-muted">©<?php echo date("Y"); ?> FlashChord.com | <a href="https://www.kcarlile.com/">Kenny Carlile</a> | <a href="/donate">Support Flash Chord</a>
