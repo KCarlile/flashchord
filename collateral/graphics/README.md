abbdb00b0e8068e3711efac554f9aca73b60f040
Kenny Carlile <kennycarlile@gmail.com>
2023-08-22 10:01:24 -0700

    Updating logo files and favicons.

---
+++ collateral/graphics/README.md
@@ -0,0 +1,2 @@
+Use the following URL to convert favicon.png to favicon.ico:
+https://www.freeconvert.com/png-to-ico
--- components/header.php
+++ components/header.php
@@ -7,8 +7,8 @@
 ?>
 <header class="d-flex flex-wrap justify-content-center py-3 mb-4 border-bottom">
     <a href="/" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto link-body-emphasis text-decoration-none">
-        <svg class="bi me-2" width="40" height="32"><use xlink:href="#bootstrap"/></svg>
-        <span class="fs-4">Flash Chord</span>
+        <svg class="me-2" width="256" height="40"><use xlink:href="/images/svg/flash-chord-logo.svg#Flash_Chord_Logo"/></svg>
+        <span class="fs-4 visually-hidden">Flash Chord</span>
     </a>
     <?php require 'components/navigation.php'; ?>
 </header>
