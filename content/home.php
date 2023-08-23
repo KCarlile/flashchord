71d01e7740507f1ee686f21e72c2a2f835eb06f9
Kenny Carlile <kennycarlile@gmail.com>
2023-08-23 16:06:33 -0700

    Tweaking styling for Settings & Instructions flyout.

--- content/home.php
+++ content/home.php
@@ -7,14 +7,14 @@
 ?>
 <div>
     <p class="lead text-muted">Practice chords, scales, or arpeggios with an endless stream of randomized chords!</p>
-    <button class="btn btn-outline-primary" type="button" data-bs-toggle="offcanvas" data-bs-target="#flash-chord-settings" aria-controls="flash-chord-settings">Show Settings &amp; Instructions</button>
+    <button class="btn btn-secondary btn-lg" type="button" data-bs-toggle="offcanvas" data-bs-target="#flash-chord-settings" aria-controls="flash-chord-settings"><i class="bi bi-gear me-2"></i>Settings &amp; Instructions</button>
 </div>
 <?php require 'content/flash-chord-player.php'; ?>
 <div class="offcanvas offcanvas-start" data-bs-backdrop="static" tabindex="-1" id="flash-chord-settings" aria-labelledby="flash-chord-settings-label">
         <div class="container">
         <div class="offcanvas-header">
-            <h3 class="offcanvas-title" id="flash-chord-settings-label">Settings &amp; Instructions</h3>
-            <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+            <h3 class="offcanvas-title" id="flash-chord-settings-label"><i class="bi bi-gear me-2"></i>Settings &amp; Instructions</h3>
+            <button type="button btn-lg" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
         </div>
         <div class="offcanvas-body">
             <?php require 'content/flash-chord-configuration.php'; ?>
