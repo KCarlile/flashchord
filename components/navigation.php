628d9965b5be65d80131d1f166abb23cb4306e6e
Kenny Carlile <kennycarlile@gmail.com>
2023-08-23 21:39:03 -0700

    Reinstating old styles for config, fixing layout issues on config flyout.

--- components/navigation.php
+++ components/navigation.php
@@ -13,7 +13,7 @@
 ?>
 <nav>
     <ul class="nav nav-pills">
-        <li class="nav-item"><a href="/" class="nav-link <?php print($page_name == 'home' ? 'active' : ''); ?>" aria-current="page">Flash Chord</a></li>
+        <li class="nav-item"><a href="/" class="nav-link <?php print($page_name == 'home' ? 'active' : ''); ?>" aria-current="page">Practice</a></li>
         <li class="nav-item"><a href="/donate" class="nav-link <?php print($page_name == 'donate' ? 'active' : ''); ?>">Donate</a></li>
         <li class="nav-item"><a href="/faq" class="nav-link <?php print($page_name == 'faq' ? 'active' : ''); ?>">FAQ</a></li>
         <li class="nav-item"><a href="/about" class="nav-link <?php print($page_name == 'about' ? 'active' : ''); ?>">About</a></li>
--- content/home.php
+++ content/home.php
@@ -10,13 +10,21 @@
     <button class="btn btn-secondary btn-lg" type="button" data-bs-toggle="offcanvas" data-bs-target="#flash-chord-settings" aria-controls="flash-chord-settings"><i class="bi bi-gear me-2"></i>Settings &amp; Instructions</button>
 </div>
 <?php require 'content/flash-chord-player.php'; ?>
-<div class="offcanvas offcanvas-start" data-bs-backdrop="static" tabindex="-1" id="flash-chord-settings" aria-labelledby="flash-chord-settings-label">
+<div class="offcanvas offcanvas-start" data-bs-backdrop="static" data-bs-scroll="true" tabindex="-1" id="flash-chord-settings" aria-labelledby="flash-chord-settings-label">
+    <div class="offcanvas-header mb-3">
         <div class="container">
-        <div class="offcanvas-header">
-            <h3 class="offcanvas-title" id="flash-chord-settings-label"><i class="bi bi-gear me-2"></i>Settings &amp; Instructions</h3>
-            <button type="button btn-lg" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+            <div class="row mx-0">
+                <div class="col-11 p-0">
+                    <h3 class="offcanvas-title" id="flash-chord-settings-label"><i class="bi bi-gear me-2"></i>Settings &amp; Instructions</h3>
+                </div>
+                <div class="col-1 text-end me-0 mt-0">
+                    <button type="button" class="btn-close m-0" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+                </div>
+            </div>
         </div>
-        <div class="offcanvas-body">
+    </div>
+    <div class="offcanvas-body">
+        <div class="container">
             <?php require 'content/flash-chord-configuration.php'; ?>
             <?php require 'content/flash-chord-instructions.php'; ?>
         </div>
