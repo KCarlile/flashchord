c9c13ed5406e4ce6a23f698ea9ba04937996213d
Kenny Carlile <kennycarlile@gmail.com>
2023-09-19 00:14:31 -0700

    Adding help page.

---
+++ content/help.php
@@ -0,0 +1,45 @@
+<?php
+    /**
+     * @file content/help.php
+     * 
+     * This include file prints content for the Help page (help.php).
+     */
+?>
+<h2 class="mb-4">Help</h2>
+<div class="mb-3">
+    <h3><i class="bi bi-gear-fill me-3 text-bg-secondary p-2 fs-5"></i>Overview</h3>
+</div>
+<div>
+    <p>As an introduction, let's look at a brief overview of the configuration
+        categories so you can setup your Flash Chord practice session
+        for success!</p>
+    <ol>
+        <li><strong>Tempo</strong> - Use the slider to adjust the speed
+            of the incoming chords. Slower tempos mean slower chord
+            changes.</li>
+        <li><strong>Time Signature</strong> - Set the time signature to
+            specify the beats per measure.</li>
+        <li><strong>Key</strong> - Select the key in which you'd like
+            to practice your chords or, for a real challenge, leave the
+            key set to <em>Any</em> to see chords from <em>any
+            key</em>.</li>
+        <li><strong>Difficulty</strong> - Select the complexity of the
+            chords being generated:
+            <ol>
+                <li>Beginner: major, minor, 7, minor 7</li>
+                <li>Intermediate: maj7, 6, m6, sus2, sus4, 9, m9</li>
+                <li>Advanced: +, °, m7♭5, 7alt, 6/9, 11, 13</li>
+            </ol>
+            You can also specify if you want additional extensions (♯5,
+                ♭5, ♯9, ♭9, ♯11, ♭13) added to the chords by checking
+                the <em>Add extensions</em> checkbox.<br />If you
+                really want to kick it up a notch, check the <em>Hide
+                next chord</em> checkbox to get the next chord without
+                any warning!
+        </li>
+    </ol>
+</div>
+<div class="alert alert-primary" role="alert">
+    <p class="my-0">See the <a href="/faq">Frequently Asked Questions (FAQ)</a> page for more information
+    about Flash Chord.</p>
+</div>
---
+++ help.php
@@ -0,0 +1,28 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <?php
+        // set the current page name for conditional formatting/content used by components
+        DEFINE("PAGE_NAME", "help");
+
+        // set the page-specific part of the page title used in components/head.php
+        DEFINE("PAGE_TITLE", "Help");
+
+        // include the global head component
+        require 'components/head.php';
+
+        // include utilities file to load contents from JSON file
+        require_once("utilities.php");
+    ?>
+</head>
+<body>
+    <div class="container">
+        <?php require 'components/header.php'; ?>
+        <!-- BEGIN page content -->
+        <?php require 'content/help.php'; ?>
+        <!-- END page content -->
+        <?php require 'components/footer.php'; ?>
+    </div>
+    <?php require 'components/foot.php'; ?>
+</body>
+</html>
