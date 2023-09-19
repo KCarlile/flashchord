8a88facff3c59623bf3d1bb93cc1bb807877e821
Kenny Carlile <kennycarlile@gmail.com>
2023-09-19 00:14:04 -0700

    Updating header and footer components for new nav and tweaks.

--- components/navigation.php
+++ components/navigation.php
@@ -15,6 +15,7 @@
     <ul class="nav nav-pills">
         <li class="nav-item"><a href="/" class="nav-link <?php print($page_name == 'home' ? 'active' : ''); ?>" aria-current="page">Practice</a></li>
         <li class="nav-item"><a href="/donate" class="nav-link <?php print($page_name == 'donate' ? 'active' : ''); ?>">Donate</a></li>
+        <li class="nav-item"><a href="/help" class="nav-link <?php print($page_name == 'help' ? 'active' : ''); ?>">Help</a></li>
         <li class="nav-item"><a href="/faq" class="nav-link <?php print($page_name == 'faq' ? 'active' : ''); ?>">FAQ</a></li>
         <li class="nav-item"><a href="/about" class="nav-link <?php print($page_name == 'about' ? 'active' : ''); ?>">About</a></li>
     </ul>
